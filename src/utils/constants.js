fd30ea5139d3d8300eedc3386b3a33b9bbf52dd1
Isaac <82594024+izaqueIsrael@users.noreply.github.com>
2023-04-18 22:00:09 -0300

    mudando nome da api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,27 +8,11 @@ import logo from '../images/logo.svg';
 import trashIcon from '../images/trash.svg';
 import likeButtonActived from '../images/union.png';
 import pen from '../images/pen.svg'
-import Api from './Api';
+import Api from './api.js';
 
 export { buttonAddIcon, closeIcon, buttonEditIcon, profileImage, likeButton, logo, trashIcon, likeButtonActived, pen };
 
 // FormValidator
-export const validation = {
-  inputSelector: '.form__input',
-  inactiveButtonClass: 'modal__button_disabled',
-  inputErrorClass: 'form__input_error',
-  formDescription: 'form__description',
-  buttonTextDisabled: 'button__text_disabled',
-  submitButtonSelector: '.modal__button',
-  editButton: '#edit__button',
-  addButton: '#add__button',
-  avatarButton: '.button_avatar',
-  inputNameClass: '.form__name',
-  inputStatusClass: '.form__status',
-  inputTitleClass: '.form__title',
-  inputLinkClass: '.form__link',
-  inputAvatarClass: '.form__avatar'
-};
 
 export const formElementsToBeValidated = {
   formName: { selector: 'form__name', range: 40 },
